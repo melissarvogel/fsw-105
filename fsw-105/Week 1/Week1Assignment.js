67bd01e10328d7b04164610bf32eb4e66f73e043
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-04-07 17:08:30 -0400

    Add files via upload

---
+++ fsw-105/Week 1/Week1Assignment.js
@@ -0,0 +1,24 @@
+
+var name = "Jane Doe"
+    console.log(name)
+
+    var age = 30
+    console.log(age)
+
+    var friends = 2
+    console.log(friends)
+
+    var isThirsty = true
+    console.log(isThirsty)
+
+    var address = "123 ABC St"
+    console.log(address)
+  
+    var person = {
+        name: "Jane Doe",
+        age: 30,
+        friends: 2,
+        isThirsty: true,
+        address: "123 ABC St"
+    }
+
