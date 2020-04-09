9a313c022e9b149fdf6f338531bc2822488c9746
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-04-08 22:37:15 -0400

    Add files via upload

---
+++ fsw-105/Week 1/Week1AssignmentResumbit.js
@@ -0,0 +1,24 @@
+var name = "Melissa"
+console.log(name)
+
+var melissaPartner = "James"
+
+var groceryCart = "milk, egg, cheese, bread"
+
+
+var shopsWeekly = true
+
+
+var moneySpent = 15
+
+
+var shopper = {
+    name: "Melissa",
+    melissaPartner: "James",
+    groceryCart: "bread, cheese",
+    shopsWeekly: true,
+    moneySpent: 15, 
+    couple: function() {return this.name + "" + this.melissaPartner;
+}
+
+}
