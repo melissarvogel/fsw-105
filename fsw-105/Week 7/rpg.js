3e389d9bdb07e7682c521903e24a18d9a1f8057f
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-05-21 11:03:58 -0400

    Add files via upload

---
+++ fsw-105/Week 7/rpg.js
@@ -0,0 +1,41 @@
+const readlineSync = require('readline-sync');
+function userName(name) {
+    this.name = name;
+}
+
+console.log("Happy day! May we have your name?")
+    if(userName != null) {
+        console.log(userName + " , let's begin our Adventure!")}
+        else {
+            console.log("Don't be a stranger! Tell us your name.")
+        } 
+        
+
+    let weAsk = ask.question("We would like you to come *Enter* the Mist and join us for a little party. You may enter the Mist by pressing the w key.")
+    let counter = 20;
+    if(weAsk.includes("Enter")) {
+        console.log("You have chosen to enter the Mist with us. As you make your way toward the party, you may encounter Party Poopers. You may choose to fight these Party Poopers, or flee. If you choose to fight, you will be given weapon options. If you choose to flee, you have a 50% chance of doing so unscathed. Please press the w key to continue into the Mist.")
+    } else {
+        if(weAsk.includes('w')){
+            var i =Math.floor(Math.random() * 100)
+            if(i < 25 && i > 50 && i < 75 && i && 100, i++){
+                console.log("You have encountered a Party Pooper! What would you like to do? You can dish a *yo mama joke*(weak), or call them a *nerd*(strong), or *flee*.") 
+                if(weAsk.includes("yo mama joke")) {
+                    console.log("The Party Pooper seemed to almost be defeated, but suddenly clapped back with a joke about your shoes! What will you do?")
+                    if(weAsk.includes(nerd)) {
+                        console.log("You hit his week spot, and have defeated the Part Pooper")
+                    }
+                var j = Math.floor(Math.random() * 100)
+                    if (j > 25 && j < 50 && j> 75 && j < 100, j++){
+                            console.log("You're getting warmer! Please press the w key to continue.")}
+                    }}
+            }
+        }{
+        console.log("We are disappointed you don't want to come to our party. Please reconsider.")
+    }
+
+    
+
+
+
+
